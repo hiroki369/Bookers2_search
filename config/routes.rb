178486bb6a5a3fae4430b9711df5ecbb6fca0acf
hiroki369 <kissjoel676@gmail.com>
2020-02-24 12:49:31 +0900

    Fix comments and favorites function

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,4 @@ get "home/about"
 get "home/search"
 
 
-put 'users/follow/:user_id' => 'users#follow'
-put 'users/unfollow/:user_id' => 'users#unfollow'
-#フォローとフォローを外すアクション
-
-get 'users/follow_list/:user_id' => 'users#follow_list'
-get 'users/follower_list/:user_id' => 'users#follower_list'
-#フォロー・フォロワーの一覧ページ
-
-
 end
